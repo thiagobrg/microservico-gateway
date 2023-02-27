f09f993970a656fa43c4b912576a32b4760b710f
Thiago Guimarães <thiagobrg98@gmail.com>
2023-02-27 20:25:30 -0300

    Teste

---
+++ src/main/java/com/microservice/teste/MicroServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/teste/MicroServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
